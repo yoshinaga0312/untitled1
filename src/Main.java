18683e7aadd7ef7d03a72af6d865edf97adb2545
unknown <241021>
2025-05-09 11:51:14 +0900

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
+// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
+public class Main {
+    public static void main(String[] args) {
+
+            System.out.println("yosinaga" );
+        }
+    }
+}
