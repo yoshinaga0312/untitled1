4e052763d153fe3c85ac92b49e2d7c981a280586
241021 <241021@std.hi-jodo.ac.jp>
2025-05-14 09:58:41 +0900

    Githabにアップロードする手順をデモするためにファイルを変更した。

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
             System.out.println("yosinaga" );
+
+            for(int i = 1; i <= 5; i++){
+                System.out.println("i = " + i);
+                System.out.println("i = " + i);
         }
     }
 }
---
+++ src/yoshinaga.java
@@ -0,0 +1,5 @@
+public class yoshinaga {
+    public static void main(String[] args) {
+        System.out.println("yosinaga");
+    }
+}
