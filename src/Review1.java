1feb722f4813148950926aa2aa88f380d277fc3d
241021 <241021@std.hi-jodo.ac.jp>
2025-05-16 11:33:51 +0900

    復習問題を提出するためにファイルを変更した。

--- src/Review1.java
+++ src/Review1.java
@@ -1,6 +1,6 @@
 public class Review1 {
     public static void main(String[] args) {
-        System.out.println("こんにちは。");
+        System.out.print("こんにちは。\n");
         System.out.println("今日の天気は" + "\t晴れです。");
         System.out.print("今日は一日\n洗濯日和です。");
         System.out.println("外に干しても大丈夫そうです。");
