ec52ef98bb0ad1c701c7186ac68cdae15e54685c
241021 <241021@std.hi-jodo.ac.jp>
2025-05-16 11:26:32 +0900

    復習問題を提出するためにファイルを変更した。

---
+++ src/Review1.java
@@ -0,0 +1,11 @@
+public class Review1 {
+    public static void main(String[] args) {
+        System.out.println("こんにちは。");
+        System.out.println("今日の天気は" + "\t晴れです。");
+        System.out.print("今日は一日\n洗濯日和です。");
+        System.out.println("外に干しても大丈夫そうです。");
+        System.out.println("文字は" + "',文字列は"+"\""+"で囲みましょう。");
+    }
+}
+
+
