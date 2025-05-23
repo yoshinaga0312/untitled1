0f2f42038a49e84726a59cd57604cf73cf06a441
241021 <241021@std.hi-jodo.ac.jp>
2025-05-23 12:29:21 +0900

    復習問題を提出するためにファイルを変更した。

---
+++ src/reviews/reviews3/Email.java
@@ -0,0 +1,14 @@
+package reviews.reviews3;
+
+public class Email {
+    public void send(String to, String body) {
+        System.out.println(to + "に以下のメールを送信しました。");
+        System.out.println("件名: 【件名なし】");
+        System.out.println("本文: " + body);
+    }
+    public void send(String to, String subject, String body) {
+        System.out.println(to + "に以下のメールを送信しました。");
+        System.out.println("件名: " + subject);
+        System.out.println("本文: " + body);
+    }
+}
---
+++ src/reviews/reviews3/Review28.java
@@ -0,0 +1,20 @@
+package reviews.reviews3;
+import java.io.*;
+public class Review28 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("宛先を入力>");
+        String to = br.readLine();
+        System.out.print("件名を入力>");
+        String subject = br.readLine();
+        System.out.print("本文を入力>");
+        String body = br.readLine();
+        Email email = new Email();
+        if(subject.isEmpty()){
+            email.send(to,body);
+        }else{
+            email.send(to,subject,body);
+        }
+    }
+}
+
