cfee89b9af967f1848e4a323f4ca2c3aeda7e2ce
241021 <241021@std.hi-jodo.ac.jp>
2025-05-26 12:36:20 +0900

    復習問題を提出するためにファイルを変更した。

--- src/reviews/reviews3/KeyInput.java
+++ src/reviews/reviews3/KeyInput.java
@@ -4,9 +4,9 @@ public class KeyInput {
     public String readString(){
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String inputStr = "";
-        if(inputStr.isEmpty()){
-            inputStr = String.valueOf(inputStr);
-        }else{
+        try{
+            inputStr = br.readLine();
+        }catch(IOException e){
             System.out.println("ERROR: IOException");
         }
         return inputStr;
--- src/reviews/reviews3/Review29.java
+++ src/reviews/reviews3/Review29.java
@@ -4,11 +4,11 @@ public class Review29 {
     public static void main(String[] args) {
         KeyInput inputStr = new KeyInput();
         System.out.print("宛先を入力>");
-        String to = String.valueOf(inputStr);
+        String to = inputStr.readString();
         System.out.print("件名を入力>");
-        String subject = String.valueOf(inputStr);
+        String subject = inputStr.readString();
         System.out.print("本文を入力>");
-        String body = String.valueOf(inputStr);
+        String body = inputStr.readString();
         System.out.println(to + "に以下のメールを送信しました。");
         System.out.println("件名: " + subject);
         System.out.println("本文: " + body);
